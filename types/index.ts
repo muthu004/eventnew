c2c030d891254cd3b3bab20efc6160ed50fef4e7
Muthukumar V <www.goe2004@gmail.com>
2024-11-14 13:46:32 +0530

    changes

--- types/index.ts
+++ types/index.ts
@@ -145,8 +145,3 @@ export type CreateUserParams = {
     keysToRemove: string[]
   }
   
-  export type SearchParamProps = {
-    params: { id: string }
-    searchParams: { [key: string]: string | string[] | undefined }
-  }
-  
